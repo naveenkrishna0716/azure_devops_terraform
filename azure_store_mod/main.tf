87ad1a068287b6197604abef8432399ffde760d6
naveenkrishna0716 <naveen.krishna0716@gmail.com>
2023-08-20 11:11:44 -0400

    remove extras

--- azure_store_mod/main.tf
+++ azure_store_mod/main.tf
@@ -25,40 +25,6 @@ resource "azurerm_storage_account" "storage_mod" {
   }
 }
 
-resource "azurerm_private_dns_zone" "my_prdns" {
-  name                = "privatelink.blob.core.windows.net"
-  resource_group_name = var.resource_grp_name
-}
-
-resource "azurerm_private_dns_zone_virtual_network_link" "network_link" {
-  name                  = "vnet_link"
-  resource_group_name   = var.resource_grp_name
-  private_dns_zone_name = azurerm_private_dns_zone.my_prdns.name
-  virtual_network_id    = azurerm_virtual_network.my_terraform_network.id
-}
-
-resource "azurerm_private_endpoint" "endpoint" {
-  name                = "my_pe"
-  location            = var.rgrp_location
-  resource_group_name = var.resource_grp_name
-  subnet_id           = azurerm_subnet.my_terraform_subnet2.id 
-  # private_link_enabled_resource_id = azurerm_storage_account.my_storage_account.id
-
-  private_service_connection {
-    name                           = "pe_conn"
-    private_connection_resource_id = azurerm_storage_account.my_storage_account.id #azurerm_private_dns_zone_virtual_network_link.network_link.id
-    is_manual_connection           = false
-    subresource_names              = ["blob"]
-  }
-}
-
-resource "azurerm_private_dns_a_record" "dns_a" {
-  name                = "my_dns"
-  zone_name           = azurerm_private_dns_zone.my_prdns.name
-  resource_group_name = var.resource_grp_name
-  ttl                 = 300
-  records             = [azurerm_private_endpoint.endpoint.private_service_connection.0.private_ip_address]
-}
 
 module "vm_mod" {
     source = "./azure_modules/vm_mod"
