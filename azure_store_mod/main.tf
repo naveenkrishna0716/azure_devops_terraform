cb08a59fd309b509a6420253ece044b670a07811
naveenkrishna0716 <naveen.krishna0716@gmail.com>
2023-08-20 11:33:27 -0400

    add private endpoint

--- azure_store_mod/main.tf
+++ azure_store_mod/main.tf
@@ -17,7 +17,7 @@ resource "random_password" "password" {
 }
 
 resource "azurerm_storage_account" "storage_mod" {
-  name                          = "staccvm2"
+  name                          = "staccvm3"
   resource_group_name           = var.resource_grp_name
   location                      = var.rgrp_location
   account_tier                  = "Standard"
@@ -36,4 +36,39 @@ module "vm_mod" {
     resource_grp_name = var.resource_grp_name
     admin_usename = var.admin_usename
     my_storage_account_endpoint = azurerm_storage_account.storage_mod.primary_blob_endpoint
+}
+
+resource "azurerm_private_dns_zone" "my_prdns" {
+  name                = "privatelink.blob.core.windows.net"
+  resource_group_name = var.resource_grp_name
+}
+
+resource "azurerm_private_dns_zone_virtual_network_link" "network_link" {
+  name                  = "vnet_link1"
+  resource_group_name   = var.resource_grp_name
+  private_dns_zone_name = azurerm_private_dns_zone.my_prdns.name
+  virtual_network_id    = [module.vm_mod.vnet_id]
+}
+
+resource "azurerm_private_endpoint" "endpoint" {
+  name                = "my_pe1"
+  location            = var.rgrp_location
+  resource_group_name = var.resource_grp_name
+  subnet_id           = [module.vm_mod.subnet_id2]
+
+
+  private_service_connection {
+    name                           = "pe_conn1"
+    private_connection_resource_id = azurerm_storage_account.my_storage_account.id #azurerm_private_dns_zone_virtual_network_link.network_link.id
+    is_manual_connection           = false
+    subresource_names              = ["blob"]
+  }
+}
+
+resource "azurerm_private_dns_a_record" "dns_a" {
+  name                = "my_dns"
+  zone_name           = azurerm_private_dns_zone.my_prdns.name
+  resource_group_name = var.resource_grp_name
+  ttl                 = 300
+  records             = [azurerm_private_endpoint.endpoint.private_service_connection.0.private_ip_address]
 }
