355ae569c15e72ff735d05d95ff2b01adca8ecd4
naveenkrishna0716 <naveen.krishna0716@gmail.com>
2023-08-26 17:38:52 -0400

    change resource name

--- vnet_mod_test/main.tf
+++ vnet_mod_test/main.tf
@@ -9,7 +9,7 @@ resource "azurerm_resource_group" "test_rg_grp" {
 
 resource "azurerm_storage_account" "test_sa" {
     name                          = "staccvm5"
-    resource_group_name           = var.resource_grp_name
+    resource_group_name           = azurerm_resource_group.test_rg_grp.name
     location                      = var.rgrp_location
     account_tier                  = "Standard"
     account_replication_type      = "GRS"
@@ -23,7 +23,7 @@ module "vnet_mod" {
     source = "./vnet_mod"
     vnet_name = "test_vnet"
     rp_location = var.rgrp_location
-    resource_rp_name = var.resource_grp_name
+    resource_rp_name = azurerm_resource_group.test_rg_grp.name
     subnet_name = "test_subnet"
 
 }
